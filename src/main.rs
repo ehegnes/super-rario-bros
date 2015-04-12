483dbd08a740d65a0d9ec717e0d8dbdec13890be
Eric Hegnes <eric.hegnes@gmail.com>
2015-04-12 18:56:42 -0400

    Implement Sprite as a Trait

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,55 @@ static GRAVITY: f32 = 9.80665/2.0; // pixels per second ;)
 static GROUND_OFFSET: i32 = 24;
 static TILE_SIZE: i32 = 16;
 
-struct Sprite<'renderer> {
+trait Sprite {
+    fn texture(&self) -> &Texture;
+    fn rect(&self) -> Rect;
+    fn x(&self) -> f32;
+    fn vx(&self) -> f32;
+    #[allow(dead_code)]
+    fn y(&self) -> f32;
+    fn vy(&self) -> f32;
+    fn falling(&self) -> bool;
+    fn set_falling(&mut self, f: bool);
+    fn set_x(&mut self, x: f32);
+    fn set_y(&mut self, y: f32);
+    fn set_vx(&mut self, vx: f32);
+    fn set_vy(&mut self, vy: f32);
+    fn jump(&mut self);
+    fn move_dir(&mut self, dir: i32);
+    fn update(&mut self, kb_state: ScanCodes);
+    fn handle_coll(&mut self, dir: &str, coll_rect: Rect) {
+        let vx = self.vx();
+        let vy = self.vy();
+        if dir == "x" {
+            if vx > 0.0 { self.set_x((coll_rect.x-TILE_SIZE) as f32); }
+            if vx < 0.0 { self.set_x((coll_rect.x + coll_rect.w) as f32); }
+            self.set_vx(0.0);
+        } else if dir == "y" && self.falling() {
+            if vy > 0.0 { // moving downward
+                self.set_y((coll_rect.y-TILE_SIZE) as f32);
+                self.set_falling(false);
+            } else { // moving upward
+                self.set_y((coll_rect.y+coll_rect.h) as f32);
+            }
+            self.set_vy(0.0);
+        }
+    }
+    fn move_mutate(&mut self, dir: &str) {
+        if dir == "x" {
+            let x  = self.x();
+            let vx = self.vx();
+            self.set_x(x + vx);
+        } else if dir == "y" && self.falling() {
+            let y  = self.y();
+            let vy = self.vy();
+            self.set_y(y + vy);
+            self.set_vy(vy + GRAVITY/60.0);
+        }
+    }
+}
+
+struct Mario<'renderer> {
     texture: Texture<'renderer>,
     x: f32,
     y: f32,
@@ -40,9 +88,9 @@ struct Sprite<'renderer> {
     falling: bool,
 }
 
-impl<'renderer> Sprite<'renderer> {
-    pub fn new(path: &str, renderer: &'renderer Renderer) -> Sprite<'renderer> {
-        Sprite {
+impl<'renderer> Mario<'renderer> {
+    fn new(path: &str, renderer: &'renderer Renderer) -> Mario<'renderer> {
+        Mario {
             texture: load_image(path, renderer),
             x: 0.0,
             y: 0.0,
@@ -51,54 +99,35 @@ impl<'renderer> Sprite<'renderer> {
             falling: false,
         }
     }
+}
 
-    pub fn texture(&self) -> &Texture { &self.texture }
-    pub fn rect(&self) -> Rect { Rect::new(self.x as i32, self.y as i32, TILE_SIZE, TILE_SIZE) }
+impl<'renderer> Sprite for Mario<'renderer> {
+    fn texture(&self) -> &Texture { &self.texture }
+    fn rect(&self) -> Rect { Rect::new(self.x as i32, self.y as i32, TILE_SIZE, TILE_SIZE) }
 
     // Setter and getter functions for internal variables
-    pub fn x<T: ToPrimitive>(&mut self) -> i32      { self.x.to_i32().unwrap() }
+    fn x(&self) -> f32      { self.x }
+    fn vx(&self) -> f32         { self.vx }
     #[allow(dead_code)]
-    pub fn y<T: ToPrimitive>(&mut self) -> i32      { self.y.to_i32().unwrap() }
-    pub fn set_x<T: ToPrimitive>(&mut self, x: T)   { self.x = x.to_f32().unwrap(); }
-    pub fn set_y<T: ToPrimitive>(&mut self, y: T)   { self.y = y.to_f32().unwrap(); }
-    pub fn set_vx<T: ToPrimitive>(&mut self, vx: T) { self.vx = vx.to_f32().unwrap(); }
-    pub fn set_vy<T: ToPrimitive>(&mut self, vy: T) { self.vy = vy.to_f32().unwrap(); }
+    fn y(&self) -> f32      { self.y }
+    fn vy(&self) -> f32         { self.vy }
+    fn falling(&self) -> bool { self.falling }
+    fn set_x(&mut self, x: f32)   { self.x = x; }
+    fn set_y(&mut self, y: f32)   { self.y = y; }
+    fn set_vx(&mut self, vx: f32) { self.vx = vx; }
+    fn set_vy(&mut self, vy: f32) { self.vy = vy; }
+    fn set_falling(&mut self, f: bool){ self.falling = f; }
 
     // Auxillary functions
-    pub fn jump(&mut self) { if !self.falling { self.vy = -3.4; self.falling = true; }}
-    pub fn move_dir<T: ToPrimitive>(&mut self, dir: T) {
+    fn jump(&mut self) { if !self.falling { self.vy = -3.4; self.falling = true; }}
+    fn move_dir(&mut self, dir: i32) {
         self.vx += (dir.to_f32().unwrap())*0.02;
         let max_speed = 1.0;
         if self.vx > max_speed { self.vx = max_speed; }
         if self.vx < -max_speed { self.vx = -max_speed; }
     }
 
-    pub fn move_mutate(&mut self, dir: &str) {
-        if dir == "x" {
-            self.x += self.vx;
-        } else if dir == "y" && self.falling {
-            self.y += self.vy;
-            self.vy += GRAVITY/60.0;
-        }
-    }
-
-    pub fn handle_coll(&mut self, dir: &str, coll_rect: Rect) {
-        if dir == "x" {
-            if self.vx > 0.0 { self.set_x(coll_rect.x-TILE_SIZE); }
-            if self.vx < 0.0 { self.set_x(coll_rect.x + coll_rect.w); }
-            self.set_vx(0);
-        } else if dir == "y" && self.falling {
-            if self.vy > 0.0 { // moving downward
-                self.set_y(coll_rect.y-TILE_SIZE);
-                self.falling = false;
-            } else { // moving upward
-                self.set_y(coll_rect.y+coll_rect.h);
-            }
-            self.set_vy(0);
-        }
-    }
-
-    pub fn update(&mut self, kb_state: ScanCodes) {
+    fn update(&mut self, kb_state: ScanCodes) {
         // Friction
         if !(kb_state[&ScanCode::Left] || kb_state[&ScanCode::Right] || self.falling) {
             self.vx -= 0.2;
@@ -108,8 +137,8 @@ impl<'renderer> Sprite<'renderer> {
         }
 
         // bounds checking
-        if (self.x as i32) < 0               { self.set_x(0); self.vx = 0.0; }
-        if (self.x as i32) > WIN_X-TILE_SIZE { self.set_x(WIN_X-TILE_SIZE); }
+        if (self.x as i32) < 0               { self.set_x(0.0); self.set_vx(0.0); }
+        if (self.x as i32) > WIN_X-TILE_SIZE { self.set_x((WIN_X-TILE_SIZE) as f32); }
 
         // Reset self.falling
         self.falling = true;
@@ -133,9 +162,9 @@ fn load_map<'renderer>(filename: &str, renderer: &'renderer Renderer) -> Texture
 }
 
 fn scroll_background(x_back: &mut i32, mario: &mut Sprite) {
-    if mario.x::<i32>() > 80 {
-        *x_back += mario.x::<i32>() - 80;
-        mario.set_x(80);
+    if mario.x() > 80.0 {
+        *x_back += mario.x() as i32 - 80;
+        mario.set_x(80.0);
     }
 
     // Bounds checking
@@ -155,11 +184,11 @@ fn main() {
 
     // Load World and Mario sprites
     let world = load_image("res/world1-1.bmp", &renderer);
-    let mut mario = Sprite::new("res/mario-walking-right.bmp", &renderer);
-    mario.set_y(WIN_Y-GROUND_OFFSET-TILE_SIZE);
+    let mut mario = Mario::new("res/mario-walking-right.bmp", &renderer);
+    mario.set_y((WIN_Y-GROUND_OFFSET-TILE_SIZE) as f32);
 
-    let mut sprites: Vec<Sprite> = Vec::new();
-    sprites.push(mario);
+    let mut sprites: Vec<&mut Sprite> = Vec::new();
+    sprites.push(&mut mario as &mut Sprite);
 
     // Generate Rects to be drawn on map
     let world_rects_overlay = load_map("res/world1-1.txt", &renderer);
@@ -172,7 +201,7 @@ fn main() {
     let mut drawer = renderer.drawer();
     let _ = drawer.clear();
     let _ = drawer.copy(&world, None, None);
-    let _ = drawer.copy(sprites[0].texture(), None, Some(sprites[0].rect()));
+    let _ = drawer.copy((*sprites[0]).texture(), None, Some((*sprites[0]).rect()));
     let _ = drawer.present();
 
     // Initialize rate limiter
@@ -191,24 +220,22 @@ fn main() {
         if kb_state[&ScanCode::Right] { sprites[0].move_dir( 1); }
 
         // Background scrolling
-        scroll_background(&mut x_back, &mut sprites[0]);
+        scroll_background(&mut x_back, &mut *sprites[0]);
 
         // X and Y collision handling
         // TODO: This needs revision. The method arrays need to be properly initialized as well.
         //       Look in the 'trait-refactor' branch for current work on this subject.
-        let f1fn = [Sprite::move_mutate, Sprite::move_mutate];
-        let f2fn = [Sprite::handle_coll, Sprite::handle_coll];
-        for (count, (f1, f2)) in f1fn.iter().zip(f2fn.iter()).enumerate() {
-            let mut dir;
-            if (count+1) % 2 == 0 { dir = "x"; } else { dir = "y"; }
-            f1(&mut sprites[0], dir);
-            for rect in &*world_rects {
-                let rect = rect.unwrap();
-                let rect = Rect::new(rect.x - x_back, rect.y, TILE_SIZE, TILE_SIZE);
-                if rect.has_intersection(&sprites[0].rect()) {
-                    let coll_rect = rect.intersection(&sprites[0].rect()).unwrap();
-                    f2(&mut sprites[0], dir, coll_rect);
-                    break;
+        for dir in ["x", "y"].iter() {
+            for sprite in sprites.iter_mut() {
+                sprite.move_mutate(dir);
+                for rect in &*world_rects {
+                    let rect = rect.unwrap();
+                    let rect = Rect::new(rect.x - x_back, rect.y, TILE_SIZE, TILE_SIZE);
+                    if rect.has_intersection(&sprite.rect()) {
+                        let coll_rect = rect.intersection(&sprite.rect()).unwrap();
+                        sprite.handle_coll(dir, coll_rect);
+                        break;
+                    }
                 }
             }
         }
